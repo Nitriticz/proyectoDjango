874a411563ae173709822d00f8d370d713184f5b
Nitriticz <127501165+Nitriticz@users.noreply.github.com>
2023-06-25 19:57:16 -0400

    Delete purrfectDjango directory

--- purrfectDjango/apps/purrfect/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from .models import *
-
-admin.site.register(Cargo)
-admin.site.register(User)
-admin.site.register(Categoria)
-admin.site.register(Animal)
-admin.site.register(Producto)
